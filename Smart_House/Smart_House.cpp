72808157400f6f3fc822710703b745c74faabadf
Andrei_Gridin <andreigridin918@gmail.com>
2024-09-08 13:52:37 +0500

    Добавьте файлы проекта.

---
+++ Smart_House/Smart_House.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	system("chcp 1251>nul");
+	system("color 70");
+
+	system("pause>nul");
+	return 0;
+}
